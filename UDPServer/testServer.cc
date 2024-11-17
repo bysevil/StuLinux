0eee27c45556d8bf049c191d8c41333b458ff7b8
bysevil <1024530748@qq.com>
2024-11-17 12:53:44 +0800

    简单的UDP echo服务器，客户端实现了多线程收发

--- UDPServer/testServer.cc
+++ UDPServer/testServer.cc
@@ -1,5 +1,6 @@
 #include"udpserver.h"
 
+//对消息进行处理
 std::string handle(std::string&& str){
     std::cout << "handle start" << std::endl;
     std::string ret;
@@ -9,6 +10,7 @@ std::string handle(std::string&& str){
     return ret;
 }
 
+//启动服务器
 int main(int argc,char* argv[]){
     UDPServer server;
     if(argc > 1){
--- UDPServer/udpclient.cc
+++ UDPServer/udpclient.cc
@@ -1,10 +1,40 @@
 #include "udpserver.h"
 
+void sender_message(int socketfd,sockaddr_in serverAddr){
+    std::string str;
+    while (true)
+    {
+        std::cout << "cin start" << std::endl;
+        std::cin >> str;
+        std::cout << "cin end" << std::endl;
+
+        std::cout << "sendto start" << std::endl;
+        sendto(socketfd, str.c_str(), str.size(), 0, (sockaddr *)&serverAddr, sizeof serverAddr);
+        std::cout << "sendto end" << std::endl;
+    }
+}
+
+void recvr_message(int socketfd,int sizebuf){
+    sockaddr_in tmp;
+    socklen_t sizetmp;
+    char buf[sizebuf]; 
+    while (true)
+    {    
+        std::cout << "recv start" << std::endl;
+        ssize_t len = recvfrom(socketfd, buf, sizebuf, 0, (sockaddr *)&tmp, &sizetmp);
+        if (len > 0){
+            buf[len] = '\0';
+            std::cout << buf << std::endl;
+            std::cout << "recv endl" << std::endl;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     int sizebuf = 1024;
     int port = 8888;
-    std::string ip("139.9.202.143");
+    std::string ip("47.109.134.57");
     switch (argc)
     {
     case 4:
@@ -14,6 +44,7 @@ int main(int argc, char *argv[])
     case 2:
         ip = argv[1];
     }
+
     int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     sockaddr_in serverAddr;
@@ -21,31 +52,11 @@ int main(int argc, char *argv[])
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(port);
 
-    while (true)
-    {
-        std::cout << "cin start" << std::endl;
-        std::string str;
-        std::cin >> str;
-        std::cout << "cin end" << std::endl;
-        std::cout << "sendto start" << std::endl;
-        sendto(socketfd, str.c_str(), str.size(), 0, (sockaddr *)&serverAddr, sizeof serverAddr);
-        std::cout << "sendto end" << std::endl;
-        sockaddr_in tmp;
-        socklen_t sizetmp;
-        char buf[sizebuf];
-
-        while (true)
-        {
-            std::cout << "recv start" << std::endl;
-            ssize_t len = recvfrom(socketfd, buf, sizebuf, 0, (sockaddr *)&tmp, &sizetmp);
-            if (len > 0){
-                buf[len] = '\0';
-                std::cout << buf << std::endl;
-                std::cout << "recv endl" << std::endl;
-                break;
-            }
-        }
-    }
+    std::thread sender(sender_message,socketfd,serverAddr);
+    std::thread recvr(recvr_message,socketfd,sizebuf);
+    
+    sender.join();
+    recvr.join();
 
     return 0;
 }
